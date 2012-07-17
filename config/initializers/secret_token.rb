033481390fd17fe7be671709d16952f5d6ed06a1
The Cypher <TheCypher@The-Cypher-Pro.local>
2012-07-17 12:11:49 -0500

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleform::Application.config.secret_token = '1b257a629073e683a203a9088c3b9a1e12c2bb4ca50ff4dbd8c7f71d2326c565444a696161fa399f4f6454c44cb7e42acce9c3de648fe670a56d4c8dcdccfddf'
